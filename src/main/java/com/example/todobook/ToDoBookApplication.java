779a5ac715ad0e89f7d80084c1ff1ecd3c83eead
Jarek Dziura <jarek4313@gmail.com>
2021-09-26 18:10:33 +0200

    [INIT] -aditing basic dependences to project

---
+++ src/main/java/com/example/todobook/ToDoBookApplication.java
@@ -0,0 +1,13 @@
+package com.example.todobook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToDoBookApplication.class, args);
+    }
+
+}
